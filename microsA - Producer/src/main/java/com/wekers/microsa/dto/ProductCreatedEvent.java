be4f7ea99f4e837f8c2ad20d64c0807dcd5594c3
wekers <fernando@wekers.org>
2025-10-30 19:04:47 -0300

    dto A

---
+++ microsA - Producer/src/main/java/com/wekers/microsa/dto/ProductCreatedEvent.java
@@ -0,0 +1,44 @@
+package com.wekers.microsa.dto;
+
+import java.io.Serializable;
+
+public class ProductCreatedEvent implements Serializable {
+    private Long id;
+    private String name;
+    private double price;
+    private String description;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
