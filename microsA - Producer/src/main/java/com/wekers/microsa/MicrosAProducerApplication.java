46feeab1b65dc348541ec2fb732c27fe9ab68343
wekers <fernando@wekers.org>
2025-10-30 19:01:57 -0300

    main class

---
+++ microsA - Producer/src/main/java/com/wekers/microsa/MicrosAProducerApplication.java
@@ -0,0 +1,13 @@
+package com.wekers.microsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosAProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrosAProducerApplication.class, args);
+    }
+
+}
---
+++ microsB - Consumer/src/main/java/com/wekers/microsb/MicrosBConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.wekers.microsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosBConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrosBConsumerApplication.class, args);
+    }
+
+}
