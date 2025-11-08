33d824308bb10739abd2a11b6f713d2382207579
wekers <fernando@wekers.org>
2025-11-08 18:23:06 -0300

    Create on microsA -> all queue

--- microsA - Producer/src/main/java/com/wekers/microsa/config/RabbitConfig.java
+++ microsA - Producer/src/main/java/com/wekers/microsa/config/RabbitConfig.java
@@ -1,24 +1,34 @@
 package com.wekers.microsa.config;
 
-
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
 
+    public static final String EXCHANGE = "products.exchange";
+    public static final String DLX = "products.dlx";
+    public static final String RK_CREATED = "products.created";
+    public static final String RK_RETRY_5S = "products.retry.5s";
+    public static final String Q_MAIN = "products.queue";
+
     @Bean
     public Queue productQueue(){
-        return new Queue("products.queue", true, false, false);
+        return QueueBuilder.durable(Q_MAIN)
+                .withArgument("x-dead-letter-exchange", DLX)
+                .withArgument("x-dead-letter-routing-key", RK_RETRY_5S)
+                .build();
     }
 
     @Bean
     public TopicExchange productExchange(){
-        return new TopicExchange("products.exchange", true, false);
+        return new TopicExchange(EXCHANGE, true, false);
+    }
+
+    @Bean
+    public DirectExchange productDlx() {
+        return new DirectExchange(DLX, true, false);
     }
 
     @Bean
@@ -26,6 +36,35 @@ public class RabbitConfig {
         return BindingBuilder
                 .bind(productQueue)
                 .to(productExchange)
-                .with("products.created");
+                .with(RK_CREATED);
+    }
+
+    // Opcional: Criar também as outras filas para garantir consistência
+    @Bean
+    public Queue retry5sQueue() {
+        return QueueBuilder.durable("products.retry.5s.queue")
+                .withArgument("x-message-ttl", 5000) // 5 segundos
+                .withArgument("x-dead-letter-exchange", EXCHANGE)
+                .withArgument("x-dead-letter-routing-key", RK_CREATED)
+                .build();
+    }
+
+    @Bean
+    public Queue deadQueue() {
+        return QueueBuilder.durable("products.dead.queue").build();
+    }
+
+    @Bean
+    public Binding bindRetry(DirectExchange productDlx, Queue retry5sQueue) {
+        return BindingBuilder.bind(retry5sQueue)
+                .to(productDlx)
+                .with(RK_RETRY_5S);
+    }
+
+    @Bean
+    public Binding bindDead(DirectExchange productDlx, Queue deadQueue) {
+        return BindingBuilder.bind(deadQueue)
+                .to(productDlx)
+                .with("products.dead");
     }
 }
