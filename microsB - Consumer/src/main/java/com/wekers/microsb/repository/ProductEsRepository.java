8fce8f7a2470435333ec235bc338b0973fd716ea
wekers <fernando@wekers.org>
2025-11-12 17:10:28 -0300

    add ProductEsRepository: annotate interface with @Repository for Elasticsearch integration

--- microsB - Consumer/src/main/java/com/wekers/microsb/repository/ProductEsRepository.java
+++ microsB - Consumer/src/main/java/com/wekers/microsb/repository/ProductEsRepository.java
@@ -2,7 +2,9 @@ package com.wekers.microsb.repository;
 
 import com.wekers.microsb.document.ProductDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductEsRepository extends ElasticsearchRepository<ProductDocument, String> {
 
 }
