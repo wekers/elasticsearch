e41acefa514f66965be4d86d0b58918fc49d037c
wekers <fernando@wekers.org>
2025-11-15 11:49:01 -0300

    refactor ProductJpaRepository: update repository to use UUID as ID type and add methods for checking duplication and finding products by name and description

--- microsA - Producer/src/main/java/com/wekers/microsa/repository/ProductJpaRepository.java
+++ microsA - Producer/src/main/java/com/wekers/microsa/repository/ProductJpaRepository.java
@@ -2,6 +2,17 @@ package com.wekers.microsa.repository;
 
 import com.wekers.microsa.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ProductJpaRepository extends JpaRepository<ProductEntity, Long> {
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ProductJpaRepository extends JpaRepository<ProductEntity, UUID> {
+
+    // Verificar duplicação (nome + descrição)
+    boolean existsByNameAndDescription(String name, String description);
+
+    // Buscar um produto específico pelo nome + descrição
+    Optional<ProductEntity> findByNameAndDescription(String name, String description);
 }
