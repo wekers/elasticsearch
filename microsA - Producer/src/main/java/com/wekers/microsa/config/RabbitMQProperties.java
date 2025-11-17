3373762896564757f17c14711a9df2adb75bdc30
wekers <fernando@wekers.org>
2025-11-17 12:30:55 -0300

    refactor(RabbitMQProperties.java): remove unused Queues and RetryConfig classes, 'A' just publish with no rules

--- microsA - Producer/src/main/java/com/wekers/microsa/config/RabbitMQProperties.java
+++ microsA - Producer/src/main/java/com/wekers/microsa/config/RabbitMQProperties.java
@@ -11,42 +11,25 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "app.rabbitmq")
 public class RabbitMQProperties {
 
-    private Queues queues = new Queues();
+
     private Exchanges exchanges = new Exchanges();
     private RoutingKeys routingKeys = new RoutingKeys();
-    private RetryConfig retry = new RetryConfig();
 
-    @Getter
-    @Setter
-    public static class Queues {
-        private String main;
-        private String retry5s;
-        private String dead;
-        private String deleted;
-
-    }
 
     @Getter
     @Setter
     public static class Exchanges {
         private String main;
-        private String dlx;
+
     }
 
     @Getter
     @Setter
     public static class RoutingKeys {
         private String created;
-        private String retry5s;
-        private String dead;
         private String deleted;
         private String updated;
     }
 
-    @Getter
-    @Setter
-    public static class RetryConfig {
-        private int maxAttempts = 3;
-        private long retryDelayMs = 5000;
-    }
+
 }
