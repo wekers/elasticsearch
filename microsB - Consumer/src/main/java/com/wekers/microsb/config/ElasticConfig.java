ee59a129eb847b1b42a60f3614d1173c534c3729
wekers <fernando@wekers.org>
2025-11-17 12:32:40 -0300

    feat(ElasticConfig.java): add configuration for Elasticsearch client and object mapper

---
+++ microsB - Consumer/src/main/java/com/wekers/microsb/config/ElasticConfig.java
@@ -0,0 +1,45 @@
+package com.wekers.microsb.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        // Suporte a Instant / LocalDateTime etc
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder(
+                new HttpHost("localhost", 9200, "http")
+        ).build();
+    }
+
+    @Bean
+    public ElasticsearchTransport elasticsearchTransport(RestClient restClient,
+                                                         ObjectMapper objectMapper) {
+        JacksonJsonpMapper jsonpMapper = new JacksonJsonpMapper(objectMapper);
+        return new RestClientTransport(restClient, jsonpMapper);
+    }
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient(ElasticsearchTransport transport) {
+        return new ElasticsearchClient(transport);
+    }
+}
