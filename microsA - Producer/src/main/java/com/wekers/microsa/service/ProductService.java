e6077a4f53a125181574ffff7348795e0b54e0ec
wekers <fernando@wekers.org>
2025-11-23 17:25:38 -0300

    feat(ProductService): add queue availability check for Microservice B
    Ensure RabbitMQ queue from Microservice B is available before creating a product to prevent transaction inconsistencies.

--- microsA - Producer/src/main/java/com/wekers/microsa/service/ProductService.java
+++ microsA - Producer/src/main/java/com/wekers/microsa/service/ProductService.java
@@ -1,20 +1,24 @@
 package com.wekers.microsa.service;
 
+import com.wekers.microsa.config.RabbitQueueAvailable;
 import com.wekers.microsa.entity.ProductEntity;
 import com.wekers.microsa.repository.ProductJpaRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductJpaRepository repository;
     private final ProductProducer producer;
+    private final RabbitQueueAvailable queueAvailable;
 
     public List<ProductEntity> listAll() {
         return repository.findAll();
@@ -22,12 +26,22 @@ public class ProductService {
 
     @Transactional
     public ProductEntity create(ProductEntity product) {
+
+
         // evita duplicidade (name + description)
         repository.findByNameAndDescription(
                 product.getName(), product.getDescription()
         ).ifPresent(p -> {
             throw new IllegalArgumentException("Produto já existe (name+description)");
         });
+
+        // Se queue não está disponível, lança exceção (faz rollback)
+        if (!queueAvailable.isMicroserviceBQueueAvailable()) {
+            throw new IllegalStateException(
+                    "Microservice B não está pronto. Fila RabbitMQ não encontrada. " +
+                            "Inicie o Microservice B primeiro."
+            );
+        }
 
         ProductEntity saved = repository.save(product);
         producer.sendCreated(saved);
